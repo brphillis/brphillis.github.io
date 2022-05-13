9be554688b66aada4031ec814a0171c8d89dd4e6
BRPHILLIS <brockphillis1992@gmail.com>
2022-05-13 16:53:26 +1000

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -21,3 +21,90 @@ function toggleText() {
         text.style.display = "none";
       }
     }
+
+
+
+
+const characterthumbnail2 = document.querySelector(".characterthumbnail2");
+    	 characterthumbnail2.addEventListener( 'click', function() {
+     	characterthumbnail2.classList.toggle('is-clicked');
+
+    		});
+
+
+        function toggleText2() {
+              var text = document.getElementById("hiddentext2");
+              if (text.style.display === "none") {
+                text.style.display = "inline-block";
+              } else {
+                text.style.display = "none";
+              }
+            }
+
+
+const characterthumbnail3 = document.querySelector(".characterthumbnail3");
+    	 characterthumbnail3.addEventListener( 'click', function() {
+     	characterthumbnail3.classList.toggle('is-clicked');
+
+    		});
+
+
+        function toggleText3() {
+              var text = document.getElementById("hiddentext3");
+              if (text.style.display === "none") {
+                text.style.display = "inline-block";
+              } else {
+                text.style.display = "none";
+              }
+            }
+
+
+const characterthumbnail4 = document.querySelector(".characterthumbnail4");
+    	 characterthumbnail4.addEventListener( 'click', function() {
+     	characterthumbnail4.classList.toggle('is-clicked');
+
+    		});
+
+
+        function toggleText4() {
+              var text = document.getElementById("hiddentext4");
+              if (text.style.display === "none") {
+                text.style.display = "inline-block";
+              } else {
+                text.style.display = "none";
+              }
+            }
+
+
+const characterthumbnail5 = document.querySelector(".characterthumbnail5");
+    	 characterthumbnail5.addEventListener( 'click', function() {
+     	characterthumbnail5.classList.toggle('is-clicked');
+
+    		});
+
+
+        function toggleText5() {
+              var text = document.getElementById("hiddentext5");
+              if (text.style.display === "none") {
+                text.style.display = "inline-block";
+              } else {
+                text.style.display = "none";
+              }
+            }
+
+
+const characterthumbnail6 = document.querySelector(".characterthumbnail6");
+    	 characterthumbnail6.addEventListener( 'click', function() {
+     	characterthumbnail6.classList.toggle('is-clicked');
+
+    		});
+
+
+        function toggleText6() {
+              var text = document.getElementById("hiddentext6");
+              if (text.style.display === "none") {
+                text.style.display = "inline-block";
+              } else {
+                text.style.display = "none";
+              }
+            }
