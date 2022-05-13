93cd2de020c8800d9a517492bf923b809488d489
BRPHILLIS <brockphillis1992@gmail.com>
2022-05-13 14:30:27 +1000

    add

--- jscript.js
+++ jscript.js
@@ -4,3 +4,20 @@ hamburger.addEventListener('click', function ()  {
  this.classList.toggle('is-active');
 
 });
+
+
+const characterthumbnail = document.querySelector(".characterthumbnail");
+		  characterthumbnail.addEventListener( 'click', function() {
+ 			characterthumbnail.classList.toggle('is-clicked');
+
+		});
+
+
+function toggleText() {
+      var text = document.getElementById("hiddentext");
+      if (text.style.display === "none") {
+        text.style.display = "inline-block";
+      } else {
+        text.style.display = "none";
+      }
+    }
