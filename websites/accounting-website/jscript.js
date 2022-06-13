bf4d86c348dae90021f8f192c8e427ce01143f11
BRPHILLIS <brockphillis1992@gmail.com>
2022-06-13 18:53:01 +1000

    last

--- websites/accounting-website/jscript.js
+++ websites/accounting-website/jscript.js
@@ -1,31 +1,40 @@
-var messages=["•Traders","•Businesses","•Individuals","•Startups","•Non-Profits"];
-var rank=0;
+var messages = [
+  "•Traders",
+  "•Businesses",
+  "•Individuals",
+  "•Startups",
+  "•Non-Profits",
+];
+var rank = 0;
 
-document.getElementById("myTypewriter").addEventListener("webkitAnimationEnd", changeTxt);
+document
+  .getElementById("myTypewriter")
+  .addEventListener("webkitAnimationEnd", changeTxt);
 
-document.getElementById("myTypewriter").addEventListener("animationend", changeTxt);
+document
+  .getElementById("myTypewriter")
+  .addEventListener("animationend", changeTxt);
 
-function changeTxt(e){
+function changeTxt(e) {
   _p = this.getElementsByTagName("p")[0];
-  _p.style.webkitAnimation = 'none';
-   setTimeout(function() {
-      _p.innerHTML=messages[rank];
-      var speed =3.5*messages[rank].length/20;
-      _p.style.webkitAnimation = 'typing '+speed+'s steps(50, end), blink-caret .10s step-end infinite';
-      (rank===messages.length-1)?rank=0:rank++;
-    }, 1000);
+  _p.style.webkitAnimation = "none";
+  setTimeout(function () {
+    _p.innerHTML = messages[rank];
+    var speed = (3.5 * messages[rank].length) / 20;
+    _p.style.webkitAnimation =
+      "typing " +
+      speed +
+      "s steps(50, end), blink-caret .10s step-end infinite";
+    rank === messages.length - 1 ? (rank = 0) : rank++;
+  }, 1000);
 }
 
-
-$(function() {
-
-$(".toggle").on("click", function(){
-
-  if($(".item").hasClass("active")){
-    $(".item").removeClass("active");
-  }
-  else{
-    $(".item").addClass("active");
-  }
-})
+$(function () {
+  $(".toggle").on("click", function () {
+    if ($(".item").hasClass("active")) {
+      $(".item").removeClass("active");
+    } else {
+      $(".item").addClass("active");
+    }
+  });
 });
